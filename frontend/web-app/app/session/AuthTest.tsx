b0f77be7cbce39f81e0402592bfe555fae0768f8
adilike <adilike_francis@yahoo.com>
2023-10-21 23:57:28 +0200

    End of session 10

--- frontend/web-app/app/session/AuthTest.tsx
+++ frontend/web-app/app/session/AuthTest.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react'
 //import { UpdateAuctionTest } from '../actions/auctionActions';
 import { Button } from 'flowbite-react';
-import { UpdateAuctionTest } from '../actions/auctionActions';
+import { updateAuctionTest } from '../actions/auctionActions';
 
 export default function AuthTest() {
     const [loading, setLoading] = useState(false);
@@ -12,7 +12,7 @@ export default function AuthTest() {
     function doUpdate() {
         setResult(undefined);
         setLoading(true);
-        UpdateAuctionTest()
+        updateAuctionTest()
             .then(res => setResult(res))
             .finally(() => setLoading(false))
             
